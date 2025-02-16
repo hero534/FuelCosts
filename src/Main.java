51298db20712c6bf96f496888430cd43861ca7f5
hgold <goldsmhr@mail.uc.edu>
2025-02-16 12:43:12 -0500

    Task 02 Completed

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double tempC = 0.00;
-        double tempF = 0.00;
-        String trash = "";
-        boolean done = false;
+        double gasNum = 0.00;
+        double fuelEff = 0.00;
+        double gasPrice = 0.00;
+        double endNum = 0.00;
+        double end = 0.00;
         do {
-            System.out.println("Enter the temperature in Celsius: ");
-            if (in.hasNextDouble()) {
-                tempC = in.nextDouble();
-                tempF = tempC * 1.8 + 32;
-                done = true;
-                System.out.println("Given " + tempC + "your temperature in Fahrenheit is: " + tempF);
-            }
-            else {
-                trash = in.nextLine();
-                System.out.println("\nYou said your temperature was: " + trash);
-            }
-        }while(!done);
+            System.out.println("Enter the number of gallons of gas in the tank: ");
+            gasNum = in.nextDouble();
+        }while(gasNum <= 0);
+        do {
+            System.out.println("Enter the fuel efficiency of your car: ");
+            fuelEff = in.nextDouble();
+        }while(fuelEff <= 0);
+        do {
+            System.out.println("Enter the price of a gallon of gas: ");
+            gasPrice = in.nextDouble();
+        }while(gasPrice <= 0);
+        endNum = 100 / gasPrice;
+        System.out.println("The cost to drive 100 miles is : " + endNum);
+        end = gasNum / fuelEff;
+        System.out.println("The car can go " + end + " miles with a full tank of gas");
     }
 }
